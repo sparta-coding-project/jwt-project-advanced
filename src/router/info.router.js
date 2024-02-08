75fc1478e6e1103c77f96371ae4ff504461f13c2
lyukidon <qgx99098@gmail.com>
2024-02-08 16:56:04 +0900

    [fix] user 데이터 받아오지 못하는 문제점

--- src/router/info.router.js
+++ src/router/info.router.js
@@ -5,22 +5,27 @@ import { prisma } from "../utils/prisma/index.js";
 const router = express.Router();
 
 router.get("/user", async (req, res) => {
-  const cookie = req.cookies;
-  const {accessToken} = cookie;
+  try {
+    const cookie = req.cookies;
+    const { accessToken } = cookie;
 
-  const userData = verifyAccessToken(accessToken);
+    const userData = verifyToken(accessToken);
 
-  const specificInfo = await prisma.userInfo.findFirst({
-    where: {
-      userId: userData.userId,
-    },
-  });
-
-  if (specificInfo) {
-    return res.status(200).json({
-      message: `Welcome! ${specificInfo.username}`,
-      userInfo: specificInfo,
+    const specificInfo = await prisma.users.findFirst({
+      where: {
+        userId: userData.userId,
+      },
     });
+
+    if (specificInfo) {
+      return res.status(200).json({
+        message: `Welcome! ${specificInfo.username}`,
+        userInfo: specificInfo,
+      });
+    }
+  } catch (error) {
+    console.log(error)
+    return res.status(401).json({message: err})
   }
 });
 
